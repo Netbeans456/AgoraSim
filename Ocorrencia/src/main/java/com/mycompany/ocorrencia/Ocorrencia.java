151390b897e27b2efc4c7f7b9996320fcbf07e85
aluno <aluno@lab01-12>
2026-05-04 09:00:22 -0300

    sla

--- Ocorrencia/src/main/java/com/mycompany/ocorrencia/Ocorrencia.java
+++ Ocorrencia/src/main/java/com/mycompany/ocorrencia/Ocorrencia.java
@@ -12,15 +12,5 @@ public class Ocorrencia {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        
     }
 }
