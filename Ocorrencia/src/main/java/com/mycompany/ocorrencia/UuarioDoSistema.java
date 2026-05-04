490c572c3b2137383914136a6db09d4f0cd79d67
aluno <aluno@lab01-12>
2026-05-04 09:31:47 -0300

    sla

---
+++ Ocorrencia/src/main/java/com/mycompany/ocorrencia/UuarioDoSistema.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ocorrencia;
+
+/**
+ *
+ * @author aluno
+ */
+public class UuarioDoSistema {
+    private int idUsuario;
+    private String login;
+    private String senhas;
+    private String perfil;
+    
+    public boolean autenticarSitema(){
+        return true;
+    }
+    
+    //ID
+    public int getIdUsuario(){
+        return idUsuario;
+    }
+    public void setIdBombeiro(int _idUsuario){
+        this.idUsuario = _idUsuario;
+    }
+    
+    //login
+}
