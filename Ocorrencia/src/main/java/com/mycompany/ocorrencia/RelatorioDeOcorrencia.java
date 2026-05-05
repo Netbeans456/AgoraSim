6d718a59f2228f8c810bb2f6b99748f42eeec0cd
aluno <aluno@lab01-12>
2026-05-05 13:58:03 -0300

    sla

---
+++ Ocorrencia/src/main/java/com/mycompany/ocorrencia/RelatorioDeOcorrencia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ocorrencia;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class RelatorioDeOcorrencia {
+    private int idRelatorio;
+    private String descricao;
+    private Date dataEncerramento;
+    private int horaEncerramento;
+    private String responsavel;
+  
+}
